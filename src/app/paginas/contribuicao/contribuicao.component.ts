efd4b34d889bd83099129e9e019a38868c59ba21
Pedro Jorge <skitvandarken2013@gmail.com>
2025-11-21 12:21:57 +0100

    correcção do firebase para renderizar no modo spa

--- src/app/paginas/contribuicao/contribuicao.component.ts
+++ src/app/paginas/contribuicao/contribuicao.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MenuComponent } from '../../layout/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { MesaService } from '../../services/mesa.service';
@@ -8,7 +9,6 @@ import { MesaService } from '../../services/mesa.service';
   imports: [
     MenuComponent,
     CommonModule,
-
   ],
   templateUrl: './contribuicao.component.html',
   styleUrl: './contribuicao.component.css'
@@ -19,20 +19,26 @@ export class ContribuicaoComponent implements OnInit, OnDestroy {
   loading = true;
   unsubscribe: any;
 
-  constructor(private mesaService: MesaService) {}
+  constructor(
+    private mesaService: MesaService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit(): void {
-
-    // Atualização em tempo real
-    this.unsubscribe = this.mesaService.listenTodasMesas((data) => {
-      this.mesas = data;
+    // Só chamar Firestore se estivermos no browser
+    if (isPlatformBrowser(this.platformId)) {
+      this.unsubscribe = this.mesaService.listenTodasMesas((data) => {
+        this.mesas = data;
+        this.loading = false;
+      });
+    } else {
+      // no server (prerender) apenas evita timeout e mostra placeholder
       this.loading = false;
-    });
-
+    }
   }
 
   ngOnDestroy(): void {
-    // Parar o listener
+    // Parar o listener se houver
     if (this.unsubscribe) {
       this.unsubscribe();
     }
